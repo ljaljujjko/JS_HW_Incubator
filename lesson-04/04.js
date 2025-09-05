c9c6c408898e550faf39bb652ff576bd1534b3c0
Volha Lialiuika <ljaljujjko@rambler.ru>
2025-09-05 23:24:48 +0300

    4/4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,13 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+ let newStr = "";
+ for (let i = 0; i < str.length; i++) {
+    const letter = str[i];
+     newStr += letter
+     newStr += letter
+ }
+return newStr;
+}
+
