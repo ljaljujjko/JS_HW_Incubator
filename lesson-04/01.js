8b70b6003c97cf8ed64f5f57f2faf6fee16552f5
Volha Lialiuika <ljaljujjko@rambler.ru>
2025-09-05 22:16:44 +0300

    lesson4/1 fixed

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,15 +13,14 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
+
+
 function includesElement(array, element) {
-    for (let i = 0; array[i] < array.lenght; i++) {
-        if (array[i] === element) {
-            return true
-        } else {
-            return false
-        }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      return true; 
     }
-
+  }
+  return false; 
 }
-
 
